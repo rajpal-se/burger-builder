1cd488c2d9dbe72896f53f0e34500b0fa5b60ab2
Rajpal Singh <rajpal426a@gmail.com>
2021-08-31 15:11:57 +0530

    Responsive Navigation is Added. - Menus (Toolbar) - SideDrawer - Toggle Button
    File changes:
    new file:   src/assets/images/burger-logo.png
    modified:   src/components/Layout/Layout.js
    modified:   src/components/Layout/Layout.module.scss
    new file:   src/components/Logo/Logo.js
    new file:   src/components/Logo/Logo.module.scss
    new file:   src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
    new file:   src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.module.scss
    new file:   src/components/Navigation/NavigationItems/NavigationItems.js
    new file:   src/components/Navigation/NavigationItems/NavigationItems.module.scss
    new file:   src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
    new file:   src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.module.scss
    new file:   src/components/Navigation/SideDrawer/SideDrawer.js
    new file:   src/components/Navigation/SideDrawer/SideDrawer.module.scss
    new file:   src/components/Navigation/Toolbar/Toolbar.js
    new file:   src/components/Navigation/Toolbar/Toolbar.module.scss

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Layout.module.scss';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = props => {
+class Layout extends Component{
 
-    return (
-        <>
-            <div>ToolBar, SideDrawer, BackDrop</div>
-            <main className={classes.Content}>
-                {props.children}
-            </main>
-        </>
-    );
+    state = {
+        showSideDrawer: false
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+    sideDrawerToggleHandler = () => {
+        this.setState( prevState => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        } );
+    }
+
+    render(){
+        return (
+            <>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </>
+        );
+    }
 }
 
 export default Layout;
