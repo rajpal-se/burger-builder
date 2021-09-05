cdb91d52349778392c6da3f228a6866220bf626e
Rajpal Singh <rajpal426a@gmail.com>
2021-09-05 21:35:43 +0530

    Form Validation is Implemented.
    File changes:
    modified:   src/components/UI/Button/Button.js
    modified:   src/components/UI/Button/Button.module.scss
    modified:   src/components/UI/Input/Input.js
    modified:   src/components/UI/Input/Input.module.scss
    modified:   src/containers/Checkout/ContactData/ContactData.js

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,10 +3,9 @@ import classes from './Button.module.scss';
 
 const Button = props => (
     <button
+        disabled={props.disabled}
         className={ [ classes.Button, classes[props.btnType] ].join(' ') }
-        onClick={props.clicked}>
-            {props.children}
-        </button>
+        onClick={props.clicked}>{props.children}</button>
 )
 
 export default Button;
